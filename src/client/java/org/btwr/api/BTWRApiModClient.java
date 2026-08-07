0f4d0151e49f90f4c6564a0fe01179af99bc842f
ivan <dbowgaming@gmail.com>
2026-08-07 20:23:21 +0300

    initial push

---
+++ src/client/java/org/btwr/api/BTWRApiModClient.java
@@ -0,0 +1,8 @@
+package org.btwr.api;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BTWRApiModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
---
+++ src/main/java/org/btwr/api/BTWRApiMod.java
@@ -0,0 +1,14 @@
+package org.btwr.api;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BTWRApiMod implements ModInitializer {
+    public static final String MOD_ID = "btwr_api";
+    public static final String MOD_NAME = "BTWR: API";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {}
+}
